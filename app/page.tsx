d78798192270b7942001f292849e906449bf8801
meesdontmiss <jermsobased@gmail.com>
2025-07-29 17:30:52 -0700

    Revert button positioning - back to working layout

--- app/page.tsx
+++ app/page.tsx
@@ -178,9 +178,7 @@ export default function V2Page() {
       <div className="absolute inset-0 pointer-events-none flex flex-col items-center justify-end pb-20 sm:pb-8">
         <div className="flex flex-col items-center pointer-events-auto space-y-3 sm:space-y-4">
           <SocialLinks />
-          <div className="-mt-12 sm:mt-0">
-            <PhotoshopButton onButtonClick={handleBurst} />
-          </div>
+          <PhotoshopButton onButtonClick={handleBurst} />
         </div>
       </div>
 
